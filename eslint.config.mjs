be0ee9b0d47c90fd4236c9fb65e840d2cf47c3df
Jayantbhoj <bhojtanay@gmail.com>
2025-02-15 17:15:58 +0530

    Fix ESLint and Clerk issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,12 @@
 import globals from "globals";
-import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  {languageOptions: { globals: {...globals.browser, ...globals.node} }},
   ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
 ];
