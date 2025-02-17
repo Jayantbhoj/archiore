9a21feb96690802b8ebcc328352c5f4ae348bdb5
Jayantbhoj <bhojtanay@gmail.com>
2025-02-17 14:05:15 +0530

    logo added in head

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,27 @@ import DelayedRender from "@/components/DelayedRender";
 import Hero from "@/components/Hero";
 import Highlights from "@/components/Highlights";
 import LandingNavbar from "@/components/LandingNavbar";
+import Head from "next/head";
 
 
 export default function Home() {
   return (
+  <>
+        <Head>
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "Archiore",
+            "url": "https://archiore.com",
+            "logo": "https://archiore.com/logosmall.png"
+        }) }} />
+      </Head>
     <div className="bg-myWhite">
 
       <LandingNavbar/>
       <Hero/>
       <Highlights/>
     </div>
+    </>
   );
 }
