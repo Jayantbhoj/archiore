37cbf500eb1dfae8a613d45d72874083d928681d
Jayantbhoj <bhojtanay@gmail.com>
2025-02-17 15:02:34 +0530

    logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import { AuthProvider } from "@/context/authContext";
 import { Analytics } from '@vercel/analytics/next';
+import Head from "next/head";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -36,6 +37,15 @@ export default function RootLayout({
   return (
 
     <html lang="en">
+      <Head>
+        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({
+            "@context": "https://schema.org",
+            "@type": "Organization",
+            "name": "Archiore",
+            "url": "https://archiore.com",
+            "logo": "https://archiore.com/logosmall.png"
+        }) }} />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
           <AuthProvider>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,7 @@ import Head from "next/head";
 export default function Home() {
   return (
   <>
-        <Head>
-        <script type="application/ld+json" dangerouslySetInnerHTML={{ __html: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "Organization",
-            "name": "Archiore",
-            "url": "https://archiore.com",
-            "logo": "https://archiore.com/logosmall.png"
-        }) }} />
-      </Head>
+
     <div className="bg-myWhite">
 
       <LandingNavbar/>
